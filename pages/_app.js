46eadafeac40384246c6bd213298e2470f36a590
Franz Medrano <franz.medrano@icloud.com>
2021-07-04 00:30:57 -0400

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className='container'></main>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
